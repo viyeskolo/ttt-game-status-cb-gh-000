dbd08b03613a919861377bd909c66a5a8330edaf
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-29 01:58:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,9 +1,6 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
-# Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
   [0,1,2], #Top row
